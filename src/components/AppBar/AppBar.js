d15bfeb5a8a64430caa0c2d87eb3951929c57225
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2023-12-16 14:53:30 +0100

    routes

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,8 @@
 import { ImportContactsOutlined, Navigation } from '@mui/icons-material';
 import { useAuth } from 'hooks/useAuth';
 import { IconButton, Toolbar, Typography } from '@mui/material';
+import { UserMenu } from 'components/UserMenu';
+import { AuthNav } from 'components/AuthNav';
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
   return (
