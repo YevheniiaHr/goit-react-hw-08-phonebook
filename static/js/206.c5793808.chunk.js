2f390b5e1a60fc5dc12df784ac1d46986ee73558
YevheniiaHr <131554616+YevheniiaHr@users.noreply.github.com>
2024-01-03 15:16:53 +0000

    Deploying to gh-pages from @ YevheniiaHr/goit-react-hw-08-phonebook@cd4e2171e2d6fc963226b660de6b4a18d4b5e8ab 🚀

---
+++ static/js/206.c5793808.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,r,o){o.r(r);var n=o(2634),t=o(184);r.default=function(){return(0,t.jsx)("div",{className:"backgroundColor: green",children:(0,t.jsx)(n.Dx,{children:"Your phonebook"})})}}}]);
+//# sourceMappingURL=206.c5793808.chunk.js.map
